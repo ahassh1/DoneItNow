f33230242a48ceaa42ed076fa9035cb650922369
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-05 13:43:24 +0600

    text added

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello this is Polash!</Text>
+      <Text>Polash is a RN developer</Text>
+      <Text>I'm passoinate about software engineering field now </Text>
+
+      <Text>
+        I want to get a job on It sector on react native developer of september
+        month inshaAllah.{" "}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # DoneWithIt command that we needed
 
-## 1. npm i -g expo-cli 
+## 1. npm i -g expo-cli X don't need this command
 ## 2. npx create-expo-app DoneItNow --template blank
 ## .3 npx expo start 
